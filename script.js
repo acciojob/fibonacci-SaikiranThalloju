906fc64e47425d456915c1f20a0e4bf96834b484
SaikiranThalloju <139635423+SaikiranThalloju@users.noreply.github.com>
2023-09-09 20:04:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function fibonacci(num) {
+
+	if(num === 0){
+		return 0;
+	}
+	else if(num == 1){
+		return 1;
+	}
+
+	else{
 let a = 0;
 	let b = 1;
-	for(int i = 2;i<=50;i++){
-		let c = a+b;
+	let c;
+	for(int i = 2;i<=num;i++){
+	      c = a+b;
 		b = c;
 		a = b;
 	}
 	return c;
+	}
 }
 
 module.exports = fibonacci;
